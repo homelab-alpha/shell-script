22bb599a2985bee0fb7dcec3959ddf5baaa6084e
GJS <homelab.api@gmail.com>
2024-03-27 11:34:06 +0100

    modified:   scripts/gnome_keybindings_backup_restore.sh

--- scripts/gnome_keybindings_backup_restore.sh
+++ scripts/gnome_keybindings_backup_restore.sh
@@ -4,8 +4,8 @@
 
 # Script Name: gnome_keybindings_backup_restore.sh
 # Author: GJS (homelab-alpha)
-# Date: 2024-03-13T09:58:50Z
-# Version: 1.0
+# Date: 2024-03-27T10:32:10Z
+# Version: 1.1
 
 # Description:
 # This script allows you to easily create backups of GNOME keybindings and restore them when needed.
@@ -45,91 +45,94 @@
 # - Ensure you have appropriate permissions to access and modify GNOME keybinding configurations.
 # - It's recommended to run this script with administrative privileges (e.g., using sudo) to avoid permission issues.
 #
+
+check_create_backup_dir() {
+  backup_dir="$HOME/Backup/Gnome/Keybindings $(date +'[%b %d, %Y %H:%M:%S]')"
+  if [ ! -d "$backup_dir" ]; then
+    mkdir -p "$backup_dir"
+    echo "Backup directory created: $backup_dir"
+  fi
+}
+
+# Function to perform backup
+perform_backup() {
+  check_create_backup_dir
+
+  backup_dir="$HOME/Backup/Gnome/Keybindings $(date +'[%b %d, %Y %H:%M:%S]')"
+
+  dconf dump / >"$backup_dir/dconf.conf"
+  dconf dump /org/gnome/desktop/wm/keybindings/ >"$backup_dir/desktop_wm_keybindings"
+  dconf dump /org/gnome/mutter/keybindings/ >"$backup_dir/mutter_keybindings"
+  dconf dump /org/gnome/mutter/wayland/keybindings/ >"$backup_dir/mutter_wayland_keybindings"
+  dconf dump /org/gnome/settings-daemon/plugins/media-keys/ >"$backup_dir/settings-daemon_plugins_media_keys"
+  dconf dump /org/gnome/shell/keybindings/ >"$backup_dir/shell_keybindings"
+
+  echo "Backup completed successfully. Backup directory: $backup_dir"
+}
+
+perform_restore() {
+  backup_recovery_dir="$HOME/Backup/Gnome"
+
+  # Check for available backups
+  backups=("$backup_recovery_dir"/*)
+  if [ ${#backups[@]} -eq 0 ]; then
+    echo "No backups found."
+    exit 1
+  fi
+
+  # Let the user choose which backup to restore
+  echo "Available backups:"
+  for ((i = 0; i < ${#backups[@]}; i++)); do
+    echo "$((i + 1)). $(basename "${backups[$i]}")"
+  done
+  read -r -p "Enter the number of the backup you want to restore: " choice
+
+  # Validate user input
+  re='^[0-9]+$'
+  if ! [[ $choice =~ $re ]]; then
+    echo "Invalid input: Please enter a number."
+    exit 1
+  fi
+
+  index=$((choice - 1))
+  if [ $index -lt 0 ] || [ $index -ge ${#backups[@]} ]; then
+    echo "Invalid backup number."
+    exit 1
+  fi
+
+  selected_backup="${backups[$index]}"
+
+  # Reset keybindings configurations to default values
+  dconf reset -f /org/gnome/desktop/wm/keybindings/
+  dconf reset -f /org/gnome/mutter/keybindings/
+  dconf reset -f /org/gnome/mutter/wayland/keybindings/
+  dconf reset -f /org/gnome/settings-daemon/plugins/media-keys/
+  dconf reset -f /org/gnome/shell/keybindings/
+
+  # Insert a delay of 0.5 seconds
+  sleep 0.5
+
+  # Restore from selected backup
+  dconf load /org/gnome/desktop/wm/keybindings/ <"$selected_backup/desktop_wm_keybindings"
+  dconf load /org/gnome/mutter/keybindings/ <"$selected_backup/mutter_keybindings"
+  dconf load /org/gnome/mutter/wayland/keybindings/ <"$selected_backup/mutter_wayland_keybindings"
+  dconf load /org/gnome/settings-daemon/plugins/media-keys/ <"$selected_backup/settings-daemon_plugins_media_keys"
+  dconf load /org/gnome/shell/keybindings/ <"$selected_backup/shell_keybindings"
+
+  echo "Backup restored successfully from: $selected_backup"
+}
+
+# Main menu
+echo "Welcome to the GNOME Keybindings Backup and Restore Script."
+echo "Please choose an option:"
+echo "1. Perform Backup"
+echo "2. Perform Restore"
+read -r -p "Enter your choice (1 or 2): " choice
+
+case $choice in
+1) perform_backup ;;
+2) perform_restore ;;
+*) echo "Invalid choice. Please enter either 1 or 2." ;;
+esac
+
 # End of script
-
-# Check if the "Backup" directory exists, if not, create it
-backup_dir="$HOME/Backup/Gnome/Keybindings $(date +'%b %d, %Y - %H%M%S')"
-if [ ! -d "$backup_dir" ]; then
-  mkdir -p "$backup_dir" # Create the backup directory if it doesn't exist
-fi
-
-# Function to create a backup of keybindings
-make_backup() {
-  if dconf dump /org/gnome/desktop/wm/keybindings/ >"$backup_dir/desktop_keybindings"; then
-    echo "Backup of GNOME desktop keybindings completed."
-  else
-    echo "An error occurred while creating the backup of GNOME desktop keybindings."
-  fi
-
-  if dconf dump /org/gnome/mutter/keybindings/ >"$backup_dir/mutter_keybindings"; then
-    echo "Backup of GNOME mutter keybindings completed."
-  else
-    echo "An error occurred while creating the backup of GNOME mutter keybindings."
-  fi
-
-  if dconf dump /org/gnome/mutter/wayland/keybindings/ >"$backup_dir/mutter_wayland_keybindings"; then
-    echo "Backup of GNOME mutter wayland keybindings completed."
-  else
-    echo "An error occurred while creating the backup of GNOME mutter wayland keybindings."
-  fi
-
-  if dconf dump /org/gnome/settings-daemon/plugins/media-keys/ >"$backup_dir/media-keys"; then
-    echo "Backup of GNOME media keys completed."
-  else
-    echo "An error occurred while creating the backup of GNOME media keys."
-  fi
-
-  if dconf dump /org/gnome/shell/keybindings/ >"$backup_dir/shell_keybindings"; then
-    echo "Backup of GNOME shell keybindings completed."
-  else
-    echo "An error occurred while creating the backup of GNOME shell keybindings."
-  fi
-}
-
-# Function to restore keybindings from backup
-restore_backup() {
-  if dconf load /org/gnome/desktop/wm/keybindings/ <"$backup_dir/desktop_keybindings"; then
-    echo "Restoration of GNOME desktop keybindings completed."
-  else
-    echo "An error occurred while restoring the backup of GNOME desktop keybindings."
-  fi
-
-  if dconf load /org/gnome/mutter/keybindings/ <"$backup_dir/mutter_keybindings"; then
-    echo "Restoration of GNOME mutter keybindings completed."
-  else
-    echo "An error occurred while restoring the backup of GNOME mutter keybindings."
-  fi
-
-  if dconf load /org/gnome/mutter/wayland/keybindings/ <"$backup_dir/mutter_wayland_keybindings"; then
-    echo "Restoration of GNOME mutter wayland keybindings completed."
-  else
-    echo "An error occurred while restoring the backup of GNOME mutter wayland keybindings."
-  fi
-
-  if dconf load /org/gnome/settings-daemon/plugins/media-keys/ <"$backup_dir/media-keys"; then
-    echo "Restoration of GNOME media keys completed."
-  else
-    echo "An error occurred while restoring the backup of GNOME media keys."
-  fi
-
-  if dconf load /org/gnome/shell/keybindings/ <"$backup_dir/shell_keybindings"; then
-    echo "Restoration of GNOME shell keybindings completed."
-  else
-    echo "An error occurred while restoring the backup of GNOME shell keybindings."
-  fi
-}
-
-# Ask the user what they want to do
-echo "What do you want to do?"
-echo "1. Make a backup of GNOME keybindings"
-echo "2. Restore the backup of GNOME keybindings"
-read -rp "Choose 1 or 2: " choice
-
-# Check the user's choice
-if [ "$choice" == "1" ]; then
-  make_backup
-elif [ "$choice" == "2" ]; then
-  restore_backup
-else
-  echo "Invalid choice. Please enter 1 or 2."
-fi
