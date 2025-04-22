5ba91d53eb31e40260c0053ed3dcad08661b5629
GJS <homelab.api@gmail.com>
2025-04-22 07:58:29 +0200

    Added: InnoDB buffer pool instances configuration to improve concurrency
    - Introduced `innodb-buffer-pool-instances = 2` in `my.cnf` under performance optimizations.
    - Improves concurrency by splitting the buffer pool across instances, reducing contention.
    - Enhances performance on multi-core systems, especially with a large buffer pool.
    
    modified: scripts/new_docker_compose_file.sh
    modified: scripts/new_docker_container.sh

--- scripts/new_docker_compose_file.sh
+++ scripts/new_docker_compose_file.sh
@@ -2,8 +2,8 @@
 
 # Filename: new_docker_compose_file.sh
 # Author: GJS (homelab-alpha)
-# Date: 2025-02-11T13:18:15+01:00
-# Version: 2.0.0
+# Date: 2025-04-22T07:30:14+02:00
+# Version: 2.1.0
 
 # Description:
 # This script automates the creation of a Docker-Compose environment based
@@ -631,6 +631,11 @@ innodb-use-native-aio = 0
 # Set the size of the InnoDB buffer pool. This should be 70-80% of the available memory.
 # Larger values allow more data to be cached in memory, improving performance.
 innodb-buffer-pool-size = 2G
+
+# Split the InnoDB buffer pool into multiple instances.
+# Improves concurrency and performance in multi-core systems by reducing contention.
+# Recommended: 1 instance per 1GB of buffer pool size.
+innodb-buffer-pool-instances = 2
 
 # Configure the size of the InnoDB redo log files. Larger log files can improve
 # performance for write-heavy workloads but require more recovery time after a crash.
